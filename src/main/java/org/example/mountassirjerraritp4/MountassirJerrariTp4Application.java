18305c6edb9ef32fa0cefcec92becdc4e25c8eac
mountassir jerrari <mountassir.jerrari@usmba.ac.ma>
2025-10-20 09:45:39 +0100

    first commit

---
+++ src/main/java/org/example/mountassirjerraritp4/MountassirJerrariTp4Application.java
@@ -0,0 +1,13 @@
+package org.example.mountassirjerraritp4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MountassirJerrariTp4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MountassirJerrariTp4Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/mountassirjerraritp4/MountassirJerrariTp4ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.mountassirjerraritp4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MountassirJerrariTp4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
